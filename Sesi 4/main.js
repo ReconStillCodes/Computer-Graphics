c664c8453a5462d5299ab257640a70e85761ae41
Matthew <m.matthew.muliawan@gmail.com>
2024-09-28 18:08:47 +0700

    update sesi 4

--- Sesi 4/main.js
+++ Sesi 4/main.js
@@ -1,11 +1,15 @@
 import * as THREE from "../threejs/build/three.module.js";
 import { OrbitControls } from "../threejs/examples/jsm/controls/OrbitControls.js";
+//Import relevant module, such as three.module.js and orbitControls.js
+//Pastikan path benar
 
-var scene, renderer, camera, control;
+var scene, renderer, camera, control; //Buat global variable ini
 
 let init = () => {
+  //Create Scene
   scene = new THREE.Scene();
 
+  //Create Camera
   let w = window.innerWidth;
   let h = window.innerHeight;
 
@@ -16,10 +20,13 @@ let init = () => {
 
   camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 
+  //Create Renderer
   renderer = new THREE.WebGLRenderer();
   renderer.setSize(w, h);
+  renderer.setClearColor(0xcfcfcf);
   document.body.appendChild(renderer.domElement);
 
+  //Create Control
   control = new OrbitControls(camera, renderer.domElement);
 };
 
@@ -29,17 +36,21 @@ let render = () => {
   control.update();
 };
 
+//Memastikan bahwa init() dan renderer() dijalankan saat membuka window
 window.onload = () => {
   init();
   render();
 };
 
+//Memastikan bahwa width & height window selalu keupdate
 window.onresize = () => {
   let w = window.innerWidth;
   let h = window.innerHeight;
 
+  //Update renderer
   renderer.setSize(w, h);
 
+  //Update camera
   camera.aspect = w / h;
-  camera.updateMatrixProjection();
+  camera.updateProjectionMatrix();
 };
