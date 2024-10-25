2902bb591d061a72baab6056ff59a1f44430649d
Matthew <m.matthew.muliawan@gmail.com>
2024-10-25 10:40:49 +0700

    Add createText

--- Sesi 7/index.js
+++ Sesi 7/index.js
@@ -1,5 +1,7 @@
 import * as THREE from "../threejs/build/three.module.js";
 import { OrbitControls } from "../threejs/examples/jsm/controls/OrbitControls.js";
+import { FontLoader } from "../threejs/examples/jsm/loaders/FontLoader.js";
+import { TextGeometry } from "../threejs/examples/jsm/geometries/TextGeometry.js";
 
 var scene, camera, renderer, control;
 
@@ -10,21 +12,59 @@ const init = () => {
   let w = window.innerWidth;
   let h = window.innerHeight;
   let near = 0.1;
-  let far = 0.001;
+  let far = 1000;
   camera = new THREE.PerspectiveCamera(fov, w / h, near, far);
+  camera.position.set(0, 0, 100);
+  camera.lookAt(0, 0, 0);
 
   renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(w, h);
-  renderer.setClearColor(0xcfcfcfc);
+  renderer.setClearColor("grey");
   document.body.appendChild(renderer.domElement);
 
   control = new OrbitControls(camera, renderer.domElement);
+
+  createText("Hello World", 15, 4, 0, 0, 0);
+  //createBox();
 };
 
 const render = () => {
   requestAnimationFrame(render);
   renderer.render(scene, camera);
   control.update();
+};
+
+const createText = (text, size, height, posX, posY, posZ) => {
+  let loader = new FontLoader();
+
+  loader.load(
+    "../threejs/examples/fonts/helvetiker_regular.typeface.json",
+    (font) => {
+      let geometry = new TextGeometry(text, {
+        font: font,
+        size: size,
+        height: height,
+        //curveSegments: 12,
+        bevelEnabled: true,
+        bevelThickness: 5,
+        bevelSize: 0.1,
+        bevelOffset: 2,
+        bevelSegments: 100,
+      });
+
+      geometry.center();
+
+      let material = new THREE.MeshNormalMaterial({
+        color: "black",
+      });
+
+      let mesh = new THREE.Mesh(geometry, material);
+      console.log(mesh);
+      camera.lookAt(mesh.position);
+      mesh.position.set(posX, posY, posZ);
+      scene.add(mesh);
+    }
+  );
 };
 
 window.onload = () => {
